8caf2400afbf4d32b32fa2c1e998fcd0fe4e02ef
skillpointser <furupumop@gmail.com>
2026-04-23 12:46:46 +0300

    Initial Commit

---
+++ HotelInUA/HotelInUAApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelInUAApp.swift
+//  HotelInUA
+//
+//  Created by Ярослав Стукаленко on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct HotelInUAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
